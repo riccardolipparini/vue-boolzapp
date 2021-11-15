6e3f5eece07101a66060ef45ea909e984c79b20e
Riccardo <riccardolipparini0@gmail.com>
2021-11-15 17:26:51 +0100

    milestone 1 complete

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var chat = new Vue({
+    el:"#containerApp",
+    data:{
+        friends:[
+        {
+            name: "Michele",
+            text: "Tutto fatto!",
+            ownChat:"ciao come va?",
+            friendChat:"tutto a posto grazie!",
+            image:"../img/avatar_1.jpg"
+        },
+        {
+            name: "Paolo",
+            text: "Tutto fatto!",
+            ownChat:"ciao come va?",
+            friendChat:"tutto a posto grazie!",
+            image:"../img/avatar_2.jpg"
+        },
+        {
+            name: "Giulia",
+            text: "Tutto fatto!",
+            ownChat:"ciao come va?",
+            friendChat:"tutto a posto grazie!",
+            image:"../img/avatar_3.jpg"
+        },
+        {
+            name: "Francesca",
+            text: "Tutto fatto!",
+            ownChat:"ciao come va?",
+            friendChat:"tutto a posto grazie!",
+            image:"../img/avatar_4.jpg"
+        }
+        
+    ]
+    }
+})
