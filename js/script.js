c19fb8e9516eabad5df1c34c6cd7b8757f67e5c5
Riccardo <riccardolipparini0@gmail.com>
2021-11-16 12:27:09 +0100

    print issue

--- js/script.js
+++ js/script.js
@@ -4,32 +4,68 @@ var chat = new Vue({
         friends:[
         {
             name: "Michele",
-            text: "Tutto fatto!",
-            ownChat:"ciao come va?",
-            friendChat:"tutto a posto grazie!",
-            image:"../img/avatar_1.jpg"
+            text: "si fatto!",
+            image:"img/avatar_1.jpg",
+            messages:[
+                {
+                    ownChat:"Hai portato a spasso il cane?",
+                    ownDate: "10/01/2020 15:30:55",
+                    friendChat:"si fatto",
+                    friendDate: "10/01/2020 15:33:25"
+
+                },
+            ],
+            
+            
         },
-        {
-            name: "Paolo",
-            text: "Tutto fatto!",
-            ownChat:"ciao come va?",
-            friendChat:"tutto a posto grazie!",
-            image:"../img/avatar_2.jpg"
-        },
-        {
-            name: "Giulia",
-            text: "Tutto fatto!",
-            ownChat:"ciao come va?",
-            friendChat:"tutto a posto grazie!",
-            image:"../img/avatar_3.jpg"
-        },
-        {
-            name: "Francesca",
-            text: "Tutto fatto!",
-            ownChat:"ciao come va?",
-            friendChat:"tutto a posto grazie!",
-            image:"../img/avatar_4.jpg"
-        }
+        // {
+        //     name: "Paolo",
+        //     text: "Tutto fatto!",
+        //     ownChat:"ciao come va?",
+        //     friendChat:"tutto a posto grazie!",
+        //     image:"img/avatar_2.jpg",
+        //     messages:[
+        //         {
+        //             ownChat:"Hai portato a spasso il cane?",
+        //             friendChat:"si fatto",
+        //             ownDate: "10/01/2020 15:30:55",
+        //             friendDate: "10/01/2020 15:33:25"
+
+        //         },
+        //     ],
+        // },
+        // {
+        //     name: "Giulia",
+        //     text: "Tutto fatto!",
+        //     ownChat:"ciao come va?",
+        //     friendChat:"tutto a posto grazie!",
+        //     image:"img/avatar_3.jpg",
+        //     messages:[
+        //         {
+        //             ownChat:"Hai portato a spasso il cane?",
+        //             friendChat:"si fatto",
+        //             ownDate: "10/01/2020 15:30:55",
+        //             friendDate: "10/01/2020 15:33:25"
+
+        //         },
+        //     ],
+        // },
+        // {
+        //     name: "Francesca",
+        //     text: "Tutto fatto!",
+        //     ownChat:"ciao come va?",
+        //     friendChat:"tutto a posto grazie!",
+        //     image:"img/avatar_4.jpg",
+        //     messages:[
+        //         {
+        //             ownChat:"Hai portato a spasso il cane?",
+        //             friendChat:"si fatto",
+        //             ownDate: "10/01/2020 15:30:55",
+        //             friendDate: "10/01/2020 15:33:25"
+
+        //         },
+        //     ],
+        // }
         
     ]
     }
